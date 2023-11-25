98eeefcf2b3ac73d446477d422256bce600efcf8
Saad Ahmad Khan <59502493+Maya-kali@users.noreply.github.com>
2023-11-25 19:02:18 +0530

    F-string usage

--- class1.py
+++ class1.py
@@ -1,2 +1,22 @@
-print("hello saad") # run python file name .py
-print("pakistan") # string
+name = "Saad Ahmad Khan" # every think in the world is Object
+                         # Action perform = Method
+                         # Attribute = like color, hand , eye
+print(name) # print
+print(type(name))  # type
+print(id(name)) # physical address
+print(dir(name))
+print([i for i in dir(name) if "__" not in i])
+
+
+#names: str = "Saad Ahmad Khan" # with type str
+#print(names)
+
+names : str = 700 # 
+print(names)
+
+name: list[str] = ['a','b','c', 200] # every think in the world is Object
+print(name) # print
+print(type(name))  # type
+print(id(name)) # physical address
+print(dir(name))
+print([i for i in dir(name) if "__" not in i])
---
+++ class_2.py
@@ -0,0 +1,20 @@
+# class name start with Capital latter : Master
+# Variable name start with small latter : master
+# Static variable name start with Capital latter : Master
+
+#name : str = "Saad Ahamd Khan"
+
+# print(name)
+
+# +++++++++++++++++++++String Data Type in Python.+++++++++++++++++++++
+
+#'string text',"string text", '''string text ''', """string text """
+name : str = 'Saad Ahamd Khan'
+f_name : str = "Basheer Khan"
+
+print(type(name))
+print(name)
+
+message : str = "PIAIC student card"
+
+
